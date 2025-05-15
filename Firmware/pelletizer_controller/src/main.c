68bb4dd719b31934a7ccf2bffbf392657fd7857e
La-guajolota <adriansilpa@gmail.com>
2025-05-15 12:15:58 -0600

    Peletizer_controller first commit

---
+++ Firmware/pelletizer_controller/src/main.c
@@ -0,0 +1,16 @@
+/*
+  @author Adrián Silva Palafox
+  @date   May 2025
+*/
+#include <avr/io.h>
+
+// #define F_CPU 16000000 //*Frecuencia del microcontrolador (16 MHz)
+
+int main(void)
+{
+
+  while (1)
+  {
+
+  }
+}
